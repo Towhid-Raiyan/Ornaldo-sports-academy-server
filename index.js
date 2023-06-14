a2b65144bceab1ef65e0f13a28506f8c11962482
Towhid-Raiyan <towhid.raiyan.com>
2023-06-14 12:21:34 +0600

    client connection  added

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
-const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const app = express();
 // const stripe = require("stripe")(process.env.SECRET_KEY);
 const port = process.env.PORT || 5000;
 
+
+const corsConfig = {
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: [
+        "Content-Type",
+        "Origin",
+        "X-Requested-With",
+        "Accept",
+        "x-client-key",
+        "x-client-token",
+        "x-client-secret",
+        "Authorization",
+    ],
+    credentials: true,
+};
+
+
 //middleware
-app.use(cors());
-// app.options("*", cors(corsConfig));
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res
+            .status(401)
+            .send({ error: true, message: "Unauthorized Access!!!" });
+    }
+    const token = authorization.split(" ")[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res
+                .status(403)
+                .send({ error: true, message: "Forbidden Access!!!" });
+        }
+        req.decoded = decoded;
+        next();
+    });
+};
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cq8nopc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,13 +66,31 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    const usersCollection = client.db("ornaldoSportsDB").collection("users");
+        const classesCollection = client
+            .db("ornaldoSportsDB")
+            .collection("classes");
+        const selectedCourseCollection = client
+            .db("ornaldoSportsDB")
+            .collection("selectedCourse");
+        const paymentCollection = client
+            .db("ornaldoSportsDB")
+            .collection("payment");
+        // jwt
+        app.post("/jwt", async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+                expiresIn: "2h",
+            });
+            res.send({ token });
+        });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
